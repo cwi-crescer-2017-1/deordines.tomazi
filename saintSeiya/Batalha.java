94a07c9382d7354ddd65dd643d435d4cd6e0257d
deordines <deordines.tomazi@cwi.com.br>
2017-04-17 15:23:13 -0300

    refactor(Batalha): definir construtor da classe Batalha e alterar o modo Iniciar.

--- saintSeiya/Batalha.java
+++ saintSeiya/Batalha.java
@@ -1,37 +1,26 @@
 public class Batalha
 {
-    public void Iniciar(Saint saint1, Saint saint2)
+    private Saint saint1, saint2;
+    final double dano = 10;
+    
+    public Batalha(Saint saint1, Saint saint2)
     {
-        System.out.println(saint1.getNome() + " de " + saint1.getArmadura().getNome() + " vs " + saint2.getNome() + " de " + saint2.getArmadura().getNome());
-        
-        Saint a = DefineQuemRecebeDano(saint1, saint2);
-        
-        a.perdeVida(10);
-        
-        System.out.println(a.getNome() + " tem " + a.getVida());
-        
-        /*while(true)
-        {
-            if (saint1.getVida() == 0 || saint2.getVida() == 0)
-                break;
-                
-            a.perdeVida(10);
-        }*/
+        this.saint1 = saint1;
+        this.saint2 = saint2;
     }
     
-    public Saint DefineQuemRecebeDano(Saint saint1, Saint saint2)
+    public void Iniciar()
     {
-        int categoria1 = saint1.getCategoria().getValor();
-        int categoria2 = saint2.getCategoria().getValor();
+        int valorSaint1 = this.saint1.getArmadura().getCategoria().getValor();
+        int valorSaint2 = this.saint2.getArmadura().getCategoria().getValor();
         
-        if (categoria1 > categoria2)
-            return saint2;
-        else if (categoria1 < categoria2)
-            return saint1;
+        if(valorSaint1 >= valorSaint2)
+        {
+            this.saint2.perderVida(dano);
+        }
         else
-            return saint2;
+        {
+            this.saint1.perderVida(dano);
+        }  
     }
-
-        
-  
 }
