4dc7cffc47f3b926284cadf7bb2a056ca68e869e
deordines <deordines.tomazi@cwi.com.br>
2017-04-17 17:21:13 -0300

    feat(Saint): adicionar classes herdeiras de Saint

--- saintSeiya/BronzeSaint.java
+++ saintSeiya/BronzeSaint.java
@@ -3,5 +3,6 @@ public class BronzeSaint extends Saint
     public BronzeSaint(String nome, Armadura armadura) throws Exception
     {
         super(nome, armadura);
+        this.qtdSentidosDespertados = 5;
     }
 }
---
+++ saintSeiya/SilverSaint.java
@@ -0,0 +1,15 @@
+// C#, C++
+// public class SilverSaint : Saint
+
+public class SilverSaint extends Saint
+{
+    public SilverSaint(String nome, Armadura armadura) throws Exception
+    {
+        super(nome, armadura);
+        this.qtdSentidosDespertados = 6;
+    }
+    
+    
+    
+    
+}
