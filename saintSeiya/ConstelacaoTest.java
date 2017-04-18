125b32c55cb21ab6224138e37f55f0a3655399d6
deordines <deordines.tomazi@cwi.com.br>
2017-04-18 15:50:47 -0300

    feat(ConstelacaoTest): adicionar novos métodos para testar a implementação de golpes.

--- saintSeiya/ConstelacaoTest.java
+++ saintSeiya/ConstelacaoTest.java
@@ -13,7 +13,7 @@ public class ConstelacaoTest
         Saint dohko = new Saint("Dohko", new Armadura(new Constelacao("Libra", new Golpe[]{new Golpe("Cóleta dos Cem Dragões", 25), new Golpe("Último Dragão", 50)}), Categoria.OURO));
     }
     
-    @Test(expected=Exception.class)
+    @Test
     public void aoAdicionarUmTerceiroGolpeDeveDarErro() throws Exception
     {
        Golpe golpe1 = new Golpe("Cápsula do Poder", 25);
@@ -25,5 +25,65 @@ public class ConstelacaoTest
        leao.adicionarGolpe(golpe3);
        Armadura ouroLeao = new Armadura(leao, Categoria.OURO);
        Saint regulus = new Saint("Regulus", ouroLeao);
+    }
+    
+    @Test
+    public void adiconarUmGolpe()
+    {
+        Constelacao touro = new Constelacao("Touro");
+        Golpe grandeChifre = new Golpe("Grande Chifre", 25);
+        touro.adicionarGolpe(grandeChifre);
+        Golpe[] golpes = touro.getGolpes();
+        
+        assertEquals(grandeChifre, golpes[0]);
+        // TODO: assert null
+        assertNull(golpes[1]);
+        assertNull(golpes[2]);
+    }
+    
+    @Test
+    public void adicionarDoisGolpes()
+    {
+        Constelacao gemeos = new Constelacao("Gêmeos");
+        Golpe outraDimensao = new Golpe("Outra Dimensão", 25);
+        Golpe explosaoGalatica = new Golpe("Explosão Galática", 50);
+        gemeos.adicionarGolpe(outraDimensao);
+        gemeos.adicionarGolpe(explosaoGalatica);
+        Golpe[] golpes = gemeos.getGolpes();
+        
+        assertEquals(outraDimensao, golpes[0]);
+        assertEquals(explosaoGalatica, golpes[1]);
+    }
+    
+    @Test
+    public void adicionarTresGolpes()
+    {
+        Constelacao gemeos = new Constelacao("Gêmeos");
+        Golpe outraDimensao = new Golpe("Outra Dimensão", 25);
+        Golpe explosaoGalatica = new Golpe("Explosão Galática", 50);
+        Golpe sataImperial = new Golpe("Satã Imperial", 75);
+        gemeos.adicionarGolpe(outraDimensao);
+        gemeos.adicionarGolpe(explosaoGalatica);
+        gemeos.adicionarGolpe(sataImperial);
+        Golpe[] golpes = gemeos.getGolpes();
+        
+        assertEquals(outraDimensao, golpes[0]);
+        assertEquals(explosaoGalatica, golpes[1]);
+        assertEquals(sataImperial, golpes[2]);
+    }
+    
+    @Test(expected=ArrayIndexOutOfBoundsException.class)
+    public void adicionarQuatroGolpes()
+    {
+        Constelacao gemeos = new Constelacao("Gêmeos");
+        Golpe outraDimensao = new Golpe("Outra Dimensão", 25);
+        Golpe explosaoGalatica = new Golpe("Explosão Galática", 50);
+        Golpe sataImperial = new Golpe("Satã Imperial", 75);
+        Golpe golpeExtra = new Golpe("golpeExtra", 100);
+        gemeos.adicionarGolpe(outraDimensao);
+        gemeos.adicionarGolpe(explosaoGalatica);
+        gemeos.adicionarGolpe(sataImperial);
+        gemeos.adicionarGolpe(golpeExtra);
+        
     }
 }
