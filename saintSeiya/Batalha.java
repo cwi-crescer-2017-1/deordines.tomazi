7138e761073a91ce422243957532f77854cee20b
deordines <deordines.tomazi@cwi.com.br>
2017-04-23 19:18:42 -0300

    refactor(Batalha): adicionar alternância de turnos com base nos movimentos de cada Saint. Adicionar teste.

--- saintSeiya/Batalha.java
+++ saintSeiya/Batalha.java
@@ -1,20 +1,32 @@
 public class Batalha {
     private Saint saint1, saint2;
-    final double dano = 10;
-    
+
     public Batalha(Saint saint1, Saint saint2) {
         this.saint1 = saint1;
         this.saint2 = saint2;
     }
-    
+
     public void iniciar() throws Exception {
         int valorSaint1 = this.saint1.getArmadura().getCategoria().getValor();
         int valorSaint2 = this.saint2.getArmadura().getCategoria().getValor();
         
-        if(valorSaint1 >= valorSaint2) {
-            this.saint2.perderVida(dano);
-        } else {
-            this.saint1.perderVida(dano);
-        }  
+        while (true) {
+            if (valorSaint1 >= valorSaint2) {
+                this.saint1.getProximoMovimento().executar();
+                if (!statusDosSaintsEmBatalha()) { break; }
+                this.saint2.getProximoMovimento().executar();
+                if (!statusDosSaintsEmBatalha()) { break; }
+            } else {
+                this.saint2.getProximoMovimento().executar();
+                if (!statusDosSaintsEmBatalha()) { break; }
+                this.saint1.getProximoMovimento().executar();
+                if (!statusDosSaintsEmBatalha()) { break; }
+            }
+        }
+    }
+
+    private boolean statusDosSaintsEmBatalha() {
+        boolean saintsEstaoVivos = this.saint1.getStatus() == Status.VIVO && this.saint2.getStatus() == Status.VIVO;
+        return saintsEstaoVivos;
     }
 }
--- saintSeiya/BatalhaTest.java
+++ saintSeiya/BatalhaTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 
 public class BatalhaTest
 {
-    @Test
+    @Test(expected=Exception.class)
     public void categoriaSaint1MaiorQueSaint2() throws Exception {
         Saint shaina = new SilverSaint("Shaina", "Serpente");        
         Saint hyoga = new BronzeSaint("Hyoga", "Cisne");
@@ -19,7 +19,7 @@ public class BatalhaTest
         assertEquals(90, hyoga.getVida(), 0.01);      
     }
     
-    @Test
+    @Test(expected=Exception.class)
     public void categoriaSaint1MenorQueSaint2() throws Exception {
         Saint hyoga = new BronzeSaint("Hyoga", "Cisne");
         Saint camus = new GoldSaint("Camus", "Aquário");
@@ -31,7 +31,7 @@ public class BatalhaTest
         assertEquals(100, camus.getVida(), 0.01);
     }
     
-    @Test
+    @Test(expected=Exception.class)
     public void categoriaSaint1IgualAoSaint2() throws Exception {
         Saint dohko = new GoldSaint("Dohko", "Libra");
         Saint shion = new GoldSaint("Shion", "Áries");
@@ -41,5 +41,36 @@ public class BatalhaTest
         
         assertEquals(100, dohko.getVida(), 0.001);
         assertEquals(90, shion.getVida(), 0.001);
+    }
+    
+    @Test
+    public void iniciarBatalhaComGoldSaintEMovimentos() throws Exception {
+        Saint dohko = new GoldSaint("Dohko", "Libra");
+        Golpe coleraDoDragao = new Golpe("Cólega do Dragão", 5);
+        Golpe coleraDosCemDragoes = new Golpe("Cólera dos Cem Dragões", 8);
+        dohko.aprenderGolpe(coleraDoDragao);
+        dohko.aprenderGolpe(coleraDosCemDragoes);
+        
+        Saint shion = new GoldSaint("Shion", "Áries");
+        Golpe extincaoEstelar = new Golpe("Extinção Estelar", 5);
+        Golpe revolucaoEstelar = new Golpe("Revolução Estelar", 8);
+        shion.aprenderGolpe(extincaoEstelar);
+        shion.aprenderGolpe(revolucaoEstelar);
+        
+        Movimento dohkoVestirArmadura = new VestirArmadura(dohko);
+        Movimento golpear1 = new Golpear(dohko, shion);
+        //dohko.adicionarMovimento(dohkoVestirArmadura);
+        dohko.adicionarMovimento(golpear1);
+        
+        Movimento shionVestirArmadura = new VestirArmadura(shion);
+        Movimento golpear2 = new Golpear(shion, dohko);
+        //shion.adicionarMovimento(shionVestirArmadura);
+        shion.adicionarMovimento(golpear2);
+        
+        Batalha batalha = new Batalha(dohko, shion);
+        batalha.iniciar();
+        
+        assertEquals(Status.VIVO, dohko.getStatus());
+        assertEquals(Status.MORTO, shion.getStatus());
     }
 }
