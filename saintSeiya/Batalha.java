155c659bdd2a17c2d437d2f9d1652ad93d34b270
deordines <deordines.tomazi@cwi.com.br>
2017-04-24 15:03:44 -0300

    refactor(Batalha): retirar verificação de categoria do método iniciar para o construtor da classe. No método iniciar, remover o código duplicado e adicionar um boolean que verifica quem está atacando em cada turno. Remover o método private que verificava qual Saint estava vivo e passado o boolean para o final do while.

--- saintSeiya/Batalha.java
+++ saintSeiya/Batalha.java
@@ -2,31 +2,35 @@ public class Batalha {
     private Saint saint1, saint2;
 
     public Batalha(Saint saint1, Saint saint2) {
-        this.saint1 = saint1;
-        this.saint2 = saint2;
-    }
+        int valorSaint1 = saint1.getArmadura().getCategoria().getValor();
+        int valorSaint2 = saint2.getArmadura().getCategoria().getValor();
 
-    public void iniciar() throws Exception {
-        int valorSaint1 = this.saint1.getArmadura().getCategoria().getValor();
-        int valorSaint2 = this.saint2.getArmadura().getCategoria().getValor();
-        
-        while (true) {
-            if (valorSaint1 >= valorSaint2) {
-                this.saint1.getProximoMovimento().executar();
-                if (!statusDosSaintsEmBatalha()) { break; }
-                this.saint2.getProximoMovimento().executar();
-                if (!statusDosSaintsEmBatalha()) { break; }
-            } else {
-                this.saint2.getProximoMovimento().executar();
-                if (!statusDosSaintsEmBatalha()) { break; }
-                this.saint1.getProximoMovimento().executar();
-                if (!statusDosSaintsEmBatalha()) { break; }
-            }
+        if (valorSaint1 >= valorSaint2) {
+            this.saint1 = saint1;
+            this.saint2 = saint2;
+        } else {
+            this.saint1 = saint2;
+            this.saint2 = saint1;
         }
     }
 
-    private boolean statusDosSaintsEmBatalha() {
-        boolean saintsEstaoVivos = this.saint1.getStatus() == Status.VIVO && this.saint2.getStatus() == Status.VIVO;
-        return saintsEstaoVivos;
+    public void iniciar() throws Exception {
+        boolean saint1Ataca = true;
+        
+        while (true) {
+            if (saint1Ataca) {
+                this.saint1.getProximoMovimento().executar();
+                saint1Ataca = false;
+            } else {
+                this.saint2.getProximoMovimento().executar();
+                saint1Ataca = true;
+            }
+            
+            boolean saintsEstaoVivos = this.saint1.getStatus() == Status.VIVO && this.saint2.getStatus() == Status.VIVO;
+            
+            if (!saintsEstaoVivos) {
+                break;
+            }
+        }
     }
 }
