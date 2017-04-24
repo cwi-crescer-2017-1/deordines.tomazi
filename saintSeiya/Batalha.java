1e703c333cff3943613876fcabf48209e9603bee
deordines <deordines.tomazi@cwi.com.br>
2017-04-24 15:56:18 -0300

    refactor(Batalha): remover if else do construtor da classe. Remover variáveis valorSaint1 e valorSaint2 do construtor para o método iniciar. Criar o boleano que verifica qual Saint ataca primeiro.

--- saintSeiya/Batalha.java
+++ saintSeiya/Batalha.java
@@ -2,22 +2,17 @@ public class Batalha {
     private Saint saint1, saint2;
 
     public Batalha(Saint saint1, Saint saint2) {
-        int valorSaint1 = saint1.getArmadura().getCategoria().getValor();
-        int valorSaint2 = saint2.getArmadura().getCategoria().getValor();
-
-        if (valorSaint1 >= valorSaint2) {
-            this.saint1 = saint1;
-            this.saint2 = saint2;
-        } else {
-            this.saint1 = saint2;
-            this.saint2 = saint1;
-        }
+        this.saint1 = saint1;
+        this.saint2 = saint2;
     }
 
     public void iniciar() throws Exception {
-        boolean saint1Ataca = true;
-        
-        while (true) {
+        int valorSaint1 = saint1.getArmadura().getCategoria().getValor();
+        int valorSaint2 = saint2.getArmadura().getCategoria().getValor();
+        boolean saint1Ataca = valorSaint1 >= valorSaint2;
+        boolean saintsEstaoVivos =  this.saint1.getStatus() == Status.VIVO && this.saint2.getStatus() == Status.VIVO;
+
+        while (saintsEstaoVivos) {
             if (saint1Ataca) {
                 this.saint1.getProximoMovimento().executar();
                 saint1Ataca = false;
@@ -25,12 +20,8 @@ public class Batalha {
                 this.saint2.getProximoMovimento().executar();
                 saint1Ataca = true;
             }
-            
-            boolean saintsEstaoVivos = this.saint1.getStatus() == Status.VIVO && this.saint2.getStatus() == Status.VIVO;
-            
-            if (!saintsEstaoVivos) {
-                break;
-            }
+
+            saintsEstaoVivos = this.saint1.getStatus() == Status.VIVO && this.saint2.getStatus() == Status.VIVO;
         }
     }
 }
