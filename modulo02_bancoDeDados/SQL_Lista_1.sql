b13383b158cfa81a1e61711644f3fc6d003e1ba8
deordines <deordines.tomazi@cwi.com.br>
2017-04-28 15:30:07 -0300

    doc(SQL_Lista_1): correção dos exercícios.

--- modulo02_bancoDeDados/SQL_Lista_1.sql
+++ modulo02_bancoDeDados/SQL_Lista_1.sql
@@ -1,9 +1,10 @@
 -- Exercício 1
-Select * INTO CidadeAux From Cidade;
+SELECT * INTO CidadeAux FROM Cidade;
 
 -- Exercício 2
 TRUNCATE TABLE CidadeAux;
-INSERT INTO CidadeAux SELECT * FROM Cidade WHERE UF = 'RS';
+INSERT INTO CidadeAux (IDCidade, Nome, UF) SELECT IDCidade, Nome, UF FROM Cidade WHERE UF = 'RS';
+-- Boa prática: especificar as colunas que serão inseridas, ao invés de usar o *.
 
 -- Exercício 3
 CREATE TABLE Produtos (
@@ -12,14 +13,21 @@ CREATE TABLE Produtos (
 	nomeDescritivo	varchar(100) not null,
 	dataCriacao		date not null,
 	localEstoque	varchar(10) not null,
-	quantidade		int not null,
-	preco			real not null,
-		constraint PK_Produtos primary key (idProduto);
-	)
+	quantidade		decimal(7,3) not null, -- 7 = quantidade total de números, onde 3 serão após a vírgula. Ex.: 1234,123
+	preco			decimal(9,3) not null, -- 9 = quantidade total de números, onde 3 serão após a vírgula. Ex.: 123456,123 
+		constraint PK_Produtos primary key (idProduto)
+	);
 
 -- Exercício 4
 INSERT INTO Produtos(nomeCurto, nomeDescritivo, dataCriacao, localEstoque, quantidade, preco)
-	VALUES ('HD WD', 'HD Western Digital SATA 3.5 Blue 7200rpm', GETDATE(), 'Corredor A', '50', '239.99'),
-			('GTX 1060', 'Placa de Vídeo GeForce GTX 1060 OC 6GB ASUS ROG', GETDATE(), 'Corredor B', '5', '1199.99');
+	VALUES ('HD WD', 'HD Western Digital SATA 3.5 Blue 7200rpm', GETDATE(), 'Corredor A', 50, 239.99),
+			('GTX 1060', 'Placa de Vídeo GeForce GTX 1060 OC 6GB ASUS ROG', GETDATE(), 'Corredor B', 5, 1199.99);
+
+-- Data e hora do BANCO e não do computador do cliente.
+SELECT GETDATE();
+
+-- Salvar data específica, convertendo string para data. Funciona somente no SQL Server.
+-- Código 103 = dd/mm/aaaa
+SELECT CONVERT(DATE, '13/05/2017', 103);
 
 SELECT * FROM Produtos;
