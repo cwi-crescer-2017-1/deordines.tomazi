81cca6e3b0934a9a6c706c19b64c4a641f3be1f9
deordines <deordines.tomazi@cwi.com.br>
2017-04-28 00:40:09 -0300

    feat(AtaqueDuplo): adicionar classe que executa ataque duplo do saint. Adicionar cenários de teste.

---
+++ modulo01_pooJava/saintSeiya/AtaqueDuplo.java
@@ -0,0 +1,35 @@
+import java.util.Random;
+
+public class AtaqueDuplo implements Movimento {
+    
+    private Saint golpeador, golpeado;
+    
+    public AtaqueDuplo(Saint golpeador, Saint golpeado) {
+        this.golpeador = golpeador;
+        this.golpeado = golpeado;
+    }
+    
+    public void executar() throws Exception {
+        Golpe golpe = this.golpeador.getProximoGolpe();
+        double danoCausado = golpe.getFatorDano();
+        
+        if (golpeador.getArmaduraVestida()) {
+            danoCausado *= this.golpeador.getArmadura().getCategoria().getValor() + 1;
+        }
+        
+        if (ataqueDuplo()) {
+            danoCausado *= 2;
+        } else {
+            this.golpeador.perderVida(this.golpeador.getVida() * 0.05);
+        }
+        
+        this.golpeado.perderVida(danoCausado);
+    }
+    
+    private boolean ataqueDuplo() {
+        Random random = new Random();
+        int min = 1, max = 6;
+        boolean ataqueDuploOcorre = random.nextInt(max - min + 1) + min <= 2 ? true : false;
+        return ataqueDuploOcorre;
+    }
+}
