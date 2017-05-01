72520ae6ddc04b5ab556cc7cb0bbd010ec5f2597
deordines <deordines.tomazi@cwi.com.br>
2017-05-01 01:27:23 -0300

    fix(Golpe): remover quebras de linha desnecessárias.

--- modulo01_pooJava/saintSeiya/Golpe.java
+++ modulo01_pooJava/saintSeiya/Golpe.java
@@ -3,24 +3,20 @@ public class Golpe
     private String nome;
     private double fatorDano;
 
-    public Golpe(String nome, double fatorDano)
-    {
+    public Golpe(String nome, double fatorDano) {
         this.nome = nome;
         this.fatorDano = fatorDano;
     }
 
-    public String getNome()
-    {
+    public String getNome() {
         return this.nome;
     }
 
-    public double getFatorDano()
-    {
+    public double getFatorDano() {
         return this.fatorDano;
     }
-
-    public boolean equals(Object object)
-    {
+    
+    public boolean equals(Object object) {
         Golpe outroGolpe = (Golpe)object;
         return this.nome.equals(outroGolpe.getNome()) && this.fatorDano == outroGolpe.getFatorDano();
     }
