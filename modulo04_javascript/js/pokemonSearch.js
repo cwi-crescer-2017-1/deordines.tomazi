1bbb53b73a6567487ee6320985dae3344acf7051
deordines <deordines.tomazi@cwi.com.br>
2017-05-17 02:58:08 -0300

    refactor(pokemonSearch): remover excesso de variáveis. Simplificado código usando appendChild ao invés de criar várias variáveis para separar os elementos.

--- modulo04_javascript/js/pokemonSearch.js
+++ modulo04_javascript/js/pokemonSearch.js
@@ -3,50 +3,46 @@ document.addEventListener('DOMContentLoaded', function() {
   let numeroPkmn = document.getElementById('numeroPkmn');
 
   btnPesquisar.onclick = function() {
-    // console.log('Click');
-    // console.log(numeroPkmn.value);
-
     let url = 'http://pokeapi.co/api/v2/pokemon/' + numeroPkmn.value;
     fetch(url)
       .then (response => response.json())
       .then(json => {
-      console.log(json);
-      console.log(json.sprites.front_default);
-      let div = document.getElementById('detalhesPokemon');
+        console.log(json);
 
-      let newDiv = document.createElement('div');
-      let img = document.createElement('img');
-      let h1 = document.createElement('h1');
-      let h2 = document.createElement('h2');
-      let ul = document.createElement('ul');
-      let br = document.createElement('br');
-      let hr = document.createElement('hr');
+        let div = document.getElementById('detalhesPokemon');
+        let ul = document.createElement('ul');
+        let identificacao = document.createElement('h1');
+        let img = document.createElement('img');
+        let tipos = json.types;
+        let atributos = json.stats;
 
-      let id = json.id;
-      let nome = json.name;
-      img.src = json.sprites.front_default;
-      let tipos = json.types;
-      tipos.reverse();
+        identificacao.innerHTML = `${ json.id } - ${ json.name.charAt(0).toUpperCase() + json.name.slice(1)}`;
+        img.src = json.sprites.front_default;
+        img.setAttribute('height', '20%');
+        img.setAttribute('width', '20%');
 
-      div.append(newDiv);
-      newDiv.setAttribute('id', 'pokemon');
-      newDiv.append(h1);
-      h1.append(`${ id } - ${ nome.charAt(0).toUpperCase() + nome.slice(1) }`);
-      newDiv.append(br);
-      newDiv.append(img);
-      img.setAttribute('height', '10%');
-      img.setAttribute('width', '10%');
-      newDiv.append(h2);
-      h2.append(ul);
+        div.appendChild(identificacao);
+        div.appendChild(img);
+        div.appendChild(ul);
 
-      tipos.forEach(tipo => {
-        let novaLi = document.createElement('li');
-        let textNode = document.createTextNode(tipo.type.name.charAt(0).toUpperCase() + tipo.type.name.slice(1));
-        novaLi.appendChild(textNode);
-        ul.insertBefore(novaLi, ul.childNodes[0]);
+        tipos.forEach(tipo => {
+          let li = document.createElement('li');
+          li.innerHTML = tipo.type.name.charAt(0).toUpperCase() + tipo.type.name.slice(1);
+          ul.appendChild(li);
+        });
+
+        atributos.forEach(atributo => {
+          let info = document.createElement('h3');
+          info.innerHTML = `${ atributo.stat.name.charAt(0).toUpperCase() + atributo.stat.name.slice(1) }: ${ atributo.base_stat }`;
+          div.appendChild(info);
+
+          let progresso = document.createElement('progress');
+          progresso.setAttribute('value', atributo.base_stat);
+          progresso.setAttribute('max', '255');
+          div.appendChild(progresso);
+        });
+
+        div.appendChild(document.createElement('hr'));
       });
-
-      newDiv.append(hr);
-    })
-  }
+    }
 });
