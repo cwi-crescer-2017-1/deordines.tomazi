e7648d4aa17a5c1e5629ce88196e3e701dfdf787
deordines <deordines.tomazi@cwi.com.br>
2017-05-18 16:42:35 -0300

    fix(exercicio2): atribuir método para o ng-click exibir a data convertida. Alterar o valor do placeholder.

--- modulo05_angular/Aula_02/js/controller.js
+++ modulo05_angular/Aula_02/js/controller.js
@@ -2,13 +2,9 @@ var myApp = angular.module('aula02', []);
 
 myApp.controller('Exercicio1', function($scope) {
 
-    $scope.converterData = converterData;
+    $scope.imprimirDataFormatada = imprimirDataFormatada;
 
-    function converterData() {
-        let pattern = '\(\d{2}\\d{2}\\d{4}';
-        let replace = '$1.$2.$3';
-        let dataFormatada = $scope.dataInserida.replace(pattern, replace);
-
-        $scope.dataObjeto = new Date(dataInserida);
+    function imprimirDataFormatada() {
+        $scope.dataObjeto = new Date($scope.dataInserida);
     }
 });
