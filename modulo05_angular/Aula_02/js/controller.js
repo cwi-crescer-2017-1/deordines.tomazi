c1a387623ac9e9b68bc6504fce6273727763b7ba
deordines <deordines.tomazi@cwi.com.br>
2017-05-18 17:26:01 -0300

    feat(exercicio2): adicionar resolução do exercicio 2 com filtro customizado.

--- modulo05_angular/Aula_02/js/controller.js
+++ modulo05_angular/Aula_02/js/controller.js
@@ -1,6 +1,6 @@
 var myApp = angular.module('aula02', []);
 
-myApp.controller('Exercicio1', function($scope) {
+myApp.controller('Exercicio1', function ($scope) {
 
     $scope.imprimirDataFormatada = imprimirDataFormatada;
 
@@ -8,3 +8,53 @@ myApp.controller('Exercicio1', function($scope) {
         $scope.dataObjeto = new Date($scope.dataInserida);
     }
 });
+
+myApp.filter('mascada', function () {
+    return function (nome) {
+        // if (nome === 'Nunes')
+            // return '$ Nunes $';
+            return nome.replace('Nunes', '$ { Nunes } $');
+        // else
+            // return nome;
+    }
+})
+
+myApp.controller('Exercicio2', function ($scope) {
+
+    let instrutores = [{
+        nome: 'Bernardo',
+        aula: [{
+            numero: 1,
+            nome: 'OO'
+        },
+        {
+            numero: 4,
+            nome: 'Javascript'
+        }
+        ]
+    },
+    {
+        nome: 'Nunes',
+        aula: [{
+            numero: 2,
+            nome: 'Banco de Dados I'
+        }]
+    },
+    {
+        nome: 'Pedro (PHP)',
+        aula: [{
+            numero: 3,
+            nome: 'HTML e CSS'
+        }]
+    },
+    {
+        nome: 'Zanatta',
+        aula: [{
+            numero: 5,
+            nome: 'AngularJS'
+        }]
+    }
+    ];
+
+    $scope.instrutores = instrutores;
+})
