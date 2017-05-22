ef477501ecdcb0e56d0e3fb4ebad31ae68ec4d10
deordines <deordines.tomazi@cwi.com.br>
2017-05-22 01:12:51 -0300

    feat(crud): Adicionar cadastro, alterar e excluir instrutores. Adicionar feedback visual para as ações do usuário através do toastr. Adicionar correções e implementações no código html. Reorganizar name dos formulários para exibir o ng-show para as validações.

--- modulo05_angular/Crud/js/controller.js
+++ modulo05_angular/Crud/js/controller.js
@@ -1,35 +1,122 @@
 var myApp = angular.module('Crud', []);
 
-myApp.controller('aula', function($scope) {
+myApp.controller('MainController', function($scope) {
     $scope.aulas = [];
-    let id = 0;
+    let idAulaAux = 0;
 
-    $scope.adicionar = function(nome) {
-        let nomes = $scope.aulas.map(aula => aula.nome);
-        if (nomes.some(n => n === nome)) {
-            console.log('Aula já cadastrada.'); //TODO: informar no html.
+    $scope.aulas = [{id: 0, nome:'a'}, {id: 1, nome:'b'}, {id: 2, nome:'c'}, {id: 3, nome:'d'}, {id: 4, nome: 'e'}];
+
+    $scope.adicionarAula = function(novoNome) {
+        if (angular.isUndefined(novoNome) || novoNome === null) {
+            console.log('Undefined or null');
             return;
+        } else if ($scope.aulas.length != 0) {
+            let nomes = $scope.aulas.map(aula => aula.nome);
+            if (nomes.some(n => n === novoNome)) {
+                console.log('Aula já cadastrada.'); //TODO: informar no html.
+                toastr.warning('Aula já cadastrada');
+                $scope.novaAula = {};
+                return;
+            }
         }
-
+        
         if ($scope.FormularioAula.$valid) {
-            $scope.aulas.push({id, nome});
-            $scope.novaAula = { };
-            id++;
+            $scope.aulas.push({id: idAulaAux, nome: novoNome});
+            $scope.novaAula = {};
+            idAulaAux++;
+            console.log($scope.aulas);
+            toastr.success('Aula cadastrada com sucesso');
         }
     }
 
-    $scope.alterar = function(id, novoNome) {
+    $scope.alterarAula = function(id, novoNome) {
+        if (angular.isUndefined(novoNome) || novoNome === null) {
+            toastr.warning('Insira um novo nome');
+            return;
+        }
+        toastr.success('Alteração efetuada com sucesso');
         console.log(id);
         console.log(novoNome);
         $scope.aulas[id].nome = novoNome;
-        $scope.novoNome = '';
+        $scope.alterarAulaId = {};
+        $scope.novoNome = {};
     }
 
-    $scope.excluir = function(id) {
-        console.log(id);
-        if (angular.isUndefined(id) || id === null)
+    // Mil tretas
+    $scope.excluirAula = function(aulaExcluida) {
+        if (angular.isUndefined(aulaExcluida) || aulaExcluida === null) {
+            toastr.warning('Selecione uma aula');
             return;
+        }
+        toastr.success('Aula excluída com sucesso');
+        // console.log(id);
+        // if (angular.isUndefined(id) || id === null) {
+        //     return;
+        // }
+        // $scope.aulas.splice($scope.aulas.findIndex(x => x.id === id), 1);
+        // $scope.excluirAulaId = null;
+        $scope.aulas = $scope.aulas.filter(aula => aula.id !== aulaExcluida.id);
+    }
 
-        $scope.aulas.splice(id, 1);
+    $scope.instrutores = [];
+    let idInstrutoresAux = 0;
+
+    $scope.instrutores = [
+    {id: 0,                            
+    nome: 'Deórdines',
+    sobrenome: 'Tomazi',
+    idade: 23,
+    email: 'deordines.tomazi@cwi.com.br',
+    dandoAula: true,
+    aula: [{id: 1, nome: 'oi'}, {id: 4, nome: 'a'}]}];
+
+    $scope.adicionarInstrutor = function() {
+        let nomes = $scope.instrutores.map(instrutor => instrutor.nome);
+        let emails = $scope.instrutores.map(instrutor => instrutor.email);
+
+        if (nomes.some(n => n === $scope.novoInstrutor.nome)) {
+            toastr.warning('Instrutor já cadastrado');
+            console.log('Instrutor já cadastrado');
+            $scope.novoInstrutor = {};
+            return;
+        } else if (emails.some(e => e === $scope.novoInstrutor.email)) {
+            toastr.warning('Email já cadastrado');
+            console.log('Email já cadastrado');
+            $scope.novoInstrutor = {};
+            return;
+        }
+
+        if ($scope.FormularioInstrutor.$valid) {
+            toastr.success('Instrutor cadastrado com sucesso');
+            $scope.instrutores.push(Object.assign({id: idInstrutoresAux}, angular.copy($scope.novoInstrutor)));
+            console.log(angular.copy($scope.novoInstrutor));
+            $scope.novoInstrutor = {};
+            idInstrutoresAux++;
+            console.log($scope.instrutores);
+        }
+    }
+
+    $scope.alterarInstrutor = function(instrutor) {
+        // if (angular.isUndefined(instrutorAtual.novoNome) || instrutorAtual.novoNome === null) {
+        //     return;
+        // }
+        // console.log(instrutorId);
+        // console.log(instrutorAtual.id);
+        // console.log($scope.instrutores[instrutorId]);
+
+        if ($scope.FormularioInstrutor.$valid) {
+            $scope.instrutores[instrutor.id] = instrutor;
+            $scope.instrutorAtual = {};
+        } else {
+            // incluir validações no html
+        }
+    }
+
+    $scope.excluirInstrutor = function(instrutorExcluido) {
+        if (instrutorExcluido.dandoAula) {
+            console.log('O instrutor selecionado não pode ser excluído.');
+            return;
+        }
+        $scope.instrutores = $scope.instrutores.filter(instrutor => instrutor.id !== instrutorExcluido.id);
     }
 });
