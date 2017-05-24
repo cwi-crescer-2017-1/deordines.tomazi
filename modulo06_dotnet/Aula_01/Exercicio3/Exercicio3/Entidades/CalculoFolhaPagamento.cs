e497c431fc4b9dd7810dde7e223bb65bd34f7dd2
deordines <deordines.tomazi@cwi.com.br>
2017-05-24 17:35:24 -0300

    feat(.net): arquivos da aula 1.

---
+++ modulo06_dotnet/Aula_01/Exercicio3/Exercicio3/Entidades/CalculoFolhaPagamento.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio3.Entidades
+{
+    class CalculoFolhaPagamento : IFolhaPagamento
+    {
+        public Demonstrativo GerarDemonstrativo(int horasCategoria, double salarioBase, double horasExtras, double horasDescontadas)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
