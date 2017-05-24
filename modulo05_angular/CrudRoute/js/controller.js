1e8a0359bb2f62e15983635e577afb3a0b24f965
deordines <deordines.tomazi@cwi.com.br>
2017-05-24 02:57:21 -0300

    fix(crudRoute): remover código comentado do crud antigo. Injetar aulaService no instrutorController. Colocar condição de imagem url default.

--- modulo05_angular/CrudRoute/js/controller.js
+++ modulo05_angular/CrudRoute/js/controller.js
@@ -1,4 +1,3 @@
-var myApp = angular.module('CrudRoute', []);
 var appRoute = angular.module('CrudRoute', ['ngRoute']);
 
 appRoute.config(function ($routeProvider) {
@@ -83,16 +82,19 @@ appRoute.controller('AulaController', function($scope, $routeParams, aulaService
     // }
 });
 
-appRoute.controller('InstrutorController', function($scope, $routeParams, instrutorService) {
+appRoute.controller('InstrutorController', function($scope, $routeParams, instrutorService, aulaService) {
 
-    $scope.criar = criar;
+    $scope.cadastrar = cadastrar;
     $scope.editar = editar;
     $scope.excluir = excluir;
+    $scope.instrutores = carregarListaDeInstrutores();
+    $scope.aulas = carregarListaDeAulas();
 
-    carregarListaDeInstrutores();
-
-    function criar(instrutor) {
-        instrutorService.criar(instrutor);
+    function cadastrar(instrutor) {
+        if (angular.isUndefined(instrutor.urlFoto) || instrutor.urlFoto === null) {
+            instrutor.urlFoto = 'http://hvazone.com/sodiz/adminlte/img/unknown-user.png';
+        }
+        instrutorService.cadastrar(instrutor);
         $scope.novoInstrutor = {};
     }
 
@@ -108,125 +110,13 @@ appRoute.controller('InstrutorController', function($scope, $routeParams, instru
     function carregarListaDeInstrutores() {
         $scope.instrutores = instrutorService.listar().then(response => $scope.instrutores = response.data);
     }
-});
 
-myApp.controller('MainController', function($scope) {
-    $scope.aulas = [];
-    let idAulaAux = 0;
-
-    $scope.aulas = [{id: 0, nome:'a'}, {id: 1, nome:'b'}, {id: 2, nome:'c'}, {id: 3, nome:'d'}, {id: 4, nome: 'e'}];
-
-    $scope.adicionarAula = function(novoNome) {
-        if (angular.isUndefined(novoNome) || novoNome === null) {
-            console.log('Undefined or null');
-            return;
-        } else if ($scope.aulas.length != 0) {
-            let nomes = $scope.aulas.map(aula => aula.nome);
-            if (nomes.some(n => n === novoNome)) {
-                console.log('Aula já cadastrada.'); //TODO: informar no html.
-                toastr.warning('Aula já cadastrada');
-                $scope.novaAula = {};
-                return;
-            }
-        }
-        
-        if ($scope.FormularioAula.$valid) {
-            $scope.aulas.push({id: idAulaAux, nome: novoNome});
-            $scope.novaAula = {};
-            idAulaAux++;
-            console.log($scope.aulas);
-            toastr.success('Aula cadastrada com sucesso');
-        }
+    function carregarListaDeAulas() {
+        $scope.aulas = aulaService.listar().then(response => $scope.aulas = response.data);
     }
 
-    $scope.alterarAula = function(id, novoNome) {
-        if (angular.isUndefined(novoNome) || novoNome === null) {
-            toastr.warning('Insira um novo nome');
-            return;
-        }
-        toastr.success('Alteração efetuada com sucesso');
-        console.log(id);
-        console.log(novoNome);
-        $scope.aulas[id].nome = novoNome;
-        $scope.alterarAulaId = {};
-        $scope.novoNome = {};
+    $scope.getNomeAula = function (idAula) {
+        return $scope.aulas.find(aula => aula.id === idAula);
     }
 
-    // Mil tretas
-    $scope.excluirAula = function(aulaExcluida) {
-        if (angular.isUndefined(aulaExcluida) || aulaExcluida === null) {
-            toastr.warning('Selecione uma aula');
-            return;
-        }
-        toastr.success('Aula excluída com sucesso');
-        // console.log(id);
-        // if (angular.isUndefined(id) || id === null) {
-        //     return;
-        // }
-        // $scope.aulas.splice($scope.aulas.findIndex(x => x.id === id), 1);
-        // $scope.excluirAulaId = null;
-        $scope.aulas = $scope.aulas.filter(aula => aula.id !== aulaExcluida.id);
-    }
-
-    $scope.instrutores = [];
-    let idInstrutoresAux = 0;
-
-    $scope.instrutores = [
-    {id: 0,                            
-    nome: 'Deórdines',
-    sobrenome: 'Tomazi',
-    idade: 23,
-    email: 'deordines.tomazi@cwi.com.br',
-    dandoAula: true,
-    aula: [{id: 1, nome: 'oi'}, {id: 4, nome: 'a'}]}];
-
-    $scope.adicionarInstrutor = function() {
-        let nomes = $scope.instrutores.map(instrutor => instrutor.nome);
-        let emails = $scope.instrutores.map(instrutor => instrutor.email);
-
-        if (nomes.some(n => n === $scope.novoInstrutor.nome)) {
-            toastr.warning('Instrutor já cadastrado');
-            console.log('Instrutor já cadastrado');
-            $scope.novoInstrutor = {};
-            return;
-        } else if (emails.some(e => e === $scope.novoInstrutor.email)) {
-            toastr.warning('Email já cadastrado');
-            console.log('Email já cadastrado');
-            $scope.novoInstrutor = {};
-            return;
-        }
-
-        if ($scope.FormularioInstrutor.$valid) {
-            toastr.success('Instrutor cadastrado com sucesso');
-            $scope.instrutores.push(Object.assign({id: idInstrutoresAux}, angular.copy($scope.novoInstrutor)));
-            console.log(angular.copy($scope.novoInstrutor));
-            $scope.novoInstrutor = {};
-            idInstrutoresAux++;
-            console.log($scope.instrutores);
-        }
-    }
-
-    $scope.alterarInstrutor = function(instrutor) {
-        // if (angular.isUndefined(instrutorAtual.novoNome) || instrutorAtual.novoNome === null) {
-        //     return;
-        // }
-        // console.log(instrutorId);
-        // console.log(instrutorAtual.id);
-        // console.log($scope.instrutores[instrutorId]);
-
-        if ($scope.FormularioInstrutor.$valid) {
-            $scope.instrutores[instrutor.id] = instrutor;
-            $scope.instrutorAtual = {};
-        } else {
-            // incluir validações no html
-        }
-    }
-
-    $scope.excluirInstrutor = function(instrutorExcluido) {
-        if (instrutorExcluido.dandoAula) {
-            console.log('O instrutor selecionado não pode ser excluído.');
-            return;
-        }
-        $scope.instrutores = $scope.instrutores.filter(instrutor => instrutor.id !== instrutorExcluido.id);
-    }
 });
