8b51d1fdfc3e7d57a1d8e62408048c7181b47090
deordines <deordines.tomazi@cwi.com.br>
2017-05-25 01:45:38 -0300

    fix(exercício3): alterar valor das alíquotas para porcentagem, reduzir o valor de proventos do desconto inss no método Irrf. Adicionar método Truncate para usar somente duas casas após a vírgula.

--- modulo06_dotnet/Aula_01/Exercicio3/Exercicio3/Entidades/FolhaPagamento.cs
+++ modulo06_dotnet/Aula_01/Exercicio3/Exercicio3/Entidades/FolhaPagamento.cs
@@ -8,7 +8,7 @@ namespace Exercicio3.Entidades
 {
     class FolhaPagamento : IFolhaPagamento
     {
-        private readonly double aliquota = 11;
+        private readonly double aliquota = 0.11;
 
         public Demonstrativo GerarDemonstrativo(double salarioBase, double horasCategoria, double horasExtras, double horasDescontadas)
         {
@@ -17,7 +17,7 @@ namespace Exercicio3.Entidades
             HorasCalculadas horaDescontada = new HorasCalculadas(horasDescontadas, ValorTotalHoras(horasDescontadas));
             double proventos = TotalProventos(salarioBase, horaExtra.ValorTotalHoras, horaDescontada.ValorTotalHoras);
             Desconto inss = Inss(proventos);
-            Desconto irrf = Irrf(proventos);
+            Desconto irrf = Irrf(proventos - inss.Valor);
             double descontos = TotalDescontos(inss.Valor, irrf.Valor);
             double valorLiquido = SalarioLiquido(proventos, descontos);
             Desconto fgts = Fgts(aliquota, proventos);
@@ -46,39 +46,46 @@ namespace Exercicio3.Entidades
         // Total de Proventos: A soma do Salário-base + Total Horas Extras - Total Horas Descontadas.
         public double TotalProventos(double salarioBase, double valorTotalHorasExtras, double valorTotalHorasDescontadas)
         {
-            return salarioBase + (valorTotalHorasExtras - valorTotalHorasDescontadas);
+            return salarioBase + valorTotalHorasExtras - valorTotalHorasDescontadas;
         }
 
         // INSS: Calcule o INSS com base no Total de Proventos e aplique a alíquota conforme as faixas salariais: Até R$1000,00 utilize 8%, até R$1500,00 9% e acima disso 10%.
         public Desconto Inss(double totalProventos)
         {
-            var aliquota = 0;
-            if (totalProventos < 1000)
-                aliquota = 8;
-            else if (totalProventos > 1000 && totalProventos < 1500)
-                aliquota = 9;
+            double aliquota = 0;
+            if (totalProventos <= 1000)
+                aliquota = 0.08;
+            else if (totalProventos > 1000 && totalProventos <= 1500)
+                aliquota = 0.09;
             else
-                aliquota = 10;
+                aliquota = 0.1;
 
-            return new Desconto(aliquota, totalProventos * (aliquota / 100));
+            var valor = totalProventos * aliquota;
+            //string converter = calculo.ToString("R");
+
+            //calculo = Math.Round(calculo, 2);
+
+            return new Desconto(aliquota, Truncate(valor));
         }
 
         // Imposto de renda retido na fonte (IRRF): Considere como base cálculo o Total de Proventos deduzido o INSS. Aplique a aliquota conforme as faixas: até R$1710.78 isento, até R$2563.91 7,5%, até R$3418.59 15%, até R$4271.59 22,5% e acima disso 27,5%.
         public Desconto Irrf(double totalProventos)
         {
             double aliquota = 0;
-            if (totalProventos < 1710.78)
+            if (totalProventos <= 1710.78)
                 aliquota = 0;
-            else if (totalProventos > 1710.78 && totalProventos < 2563.91)
-                aliquota = 7.5;
-            else if (totalProventos > 2563.91 && totalProventos < 3418.59)
-                aliquota = 15;
-            else if (totalProventos > 3418.59 && totalProventos < 4271.59)
-                aliquota = 22.5;
+            else if (totalProventos > 1710.78 && totalProventos <= 2563.91)
+                aliquota = 0.075;
+            else if (totalProventos > 2563.91 && totalProventos <= 3418.59)
+                aliquota = 0.15;
+            else if (totalProventos > 3418.59 && totalProventos <= 4271.59)
+                aliquota = 0.225;
             else
-                aliquota = 27.5;
+                aliquota = 0.275;
 
-            return new Desconto (aliquota, totalProventos * (aliquota / 100));
+            var valor = totalProventos * aliquota;
+
+            return new Desconto (aliquota, Truncate(valor));
         }
 
         // Total de descontos:é a soma do INSS e do IRRF.
@@ -90,13 +97,20 @@ namespace Exercicio3.Entidades
         // Salário líquido: A remuneração a ser repassada (transferida) para o colaborador. Para o cálculo some o Total de Proventos - Total de Descontos.
         public double SalarioLiquido(double totalProventos, double totalDescontos)
         {
-            return totalDescontos - totalDescontos;
+            return Truncate(totalProventos - totalDescontos);
         }
 
         // FGTS: Fundo de garantia sobre tempo de serviço deve ser apenas discriminado 11% fixo.
         public Desconto Fgts(double aliquota, double totalProventos)
         {
-            return new Desconto(aliquota, totalProventos * (aliquota / 100));
+            var valor = totalProventos * aliquota;
+
+            return new Desconto(aliquota, Truncate(valor));
+        }
+
+        public double Truncate (double valor)
+        {
+            return Math.Truncate(valor * 100) / 100;
         }
     }
 }
