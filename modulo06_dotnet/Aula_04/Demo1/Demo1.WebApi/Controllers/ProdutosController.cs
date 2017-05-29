25be7a38546a4058743c9d42b75f76d6e91d9d2e
deordines <deordines.tomazi@cwi.com.br>
2017-05-29 17:44:55 -0300

    feat(Demo1): adicionar restante da aplicação Demo1.

--- modulo06_dotnet/Aula_04/Demo1/Demo1.WebApi/Controllers/ProdutosController.cs
+++ modulo06_dotnet/Aula_04/Demo1/Demo1.WebApi/Controllers/ProdutosController.cs
@@ -1,4 +1,5 @@
 ﻿using Demo1.Dominio.Entidades;
+using Demo1.Infraestrutura.Repositorios;
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
@@ -11,47 +12,40 @@ namespace Demo1.WebApi.Controllers
 {
     public class ProdutosController : ApiController
     {
+        ProdutoRepositorio _produtoRepositorio = new ProdutoRepositorio();
+
         public IHttpActionResult Post(Produto produto)
         {
-            string stringConexao =
-                "Server=13.65.101.67;User Id=deordines.tomazi;PAssword=123456;Database=aluno03db";
-
             var mensagens = new List<string>();
             if (!produto.Validar(out mensagens))
                 return BadRequest(string.Join(".", mensagens.ToArray()));
 
-            using (var conexao = new SqlConnection(stringConexao))
-            {
-                conexao.Open();
-                using (var comando = conexao.CreateCommand())
-                {
-                    comando.CommandText = "INSERT INTO Produto(Nome, Preco, Estoque) VALUES (@nome, @preco, @estoque)";
-                    comando.Parameters.AddWithValue("@nome", produto.Nome);
-                    comando.Parameters.AddWithValue("@preco", produto.Preco);
-                    comando.Parameters.AddWithValue("@estoque", produto.Estoque);
+            _produtoRepositorio.Criar(produto);
 
-                    // Executa o comando e retorna somente a quantidade de linhas afetadas
-                    comando.ExecuteNonQuery(); // retorno int
-
-                    // Lê o resultado de um SELECT
-                    //comando.ExecuteReader();
-
-                    // Executa o comando e retorna o primeiro resultado
-                    //comando.ExecuteScalar();
-                }
-
-                using (var comando = conexao.CreateCommand())
-                {
-                    comando.CommandText = "SELECT @@IDENTITY";
-
-                    // Executa o comando e retorna o primeiro resultado
-                    var resultado = (decimal)comando.ExecuteScalar();
-                    produto.Id = (int)resultado;
-                }
-            }
-
-            // String.Empty é uma constante, repsenta uma string vazia é igual abre e fecha aspas
             return Ok(produto);
+        }
+
+        public IHttpActionResult Put(Produto produto)
+        {
+            return Ok();
+        }
+
+        public IHttpActionResult Get()
+        {
+            var produtos = _produtoRepositorio.Listar();
+
+            return Ok(produtos);
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            _produtoRepositorio.Excluir(id);
+            return Ok();
         }
     }
 }
