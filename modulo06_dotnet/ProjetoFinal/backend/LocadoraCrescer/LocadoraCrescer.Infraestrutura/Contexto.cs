8785d0a4d4eb96a9d6a560b1069dc0df2d0e46c1
deordines <deordines.tomazi@cwi.com.br>
2017-06-05 21:41:49 -0300

    feat(LocadoraCrescer): adicionar classes de Mappings e Contexto.

---
+++ modulo06_dotnet/ProjetoFinal/backend/LocadoraCrescer/LocadoraCrescer.Infraestrutura/Contexto.cs
@@ -0,0 +1,38 @@
+﻿using LocadoraCrescer.Dominio.Entidades;
+using LocadoraCrescer.Infraestrutura.Mappings;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraCrescer.Infraestrutura
+{
+    public class Contexto : DbContext
+    {
+        public Contexto() : base("name=ConnectionString")
+        {
+
+        }
+
+        public DbSet<Cliente> Cliente { get; set; }
+        public DbSet<Extra> Extra { get; set; }
+        public DbSet<Locacao> Locacao { get; set; }
+        public DbSet<Pacote> Pacote { get; set; }
+        public DbSet<ProdutoConsole> ProdutoConsole { get; set; }
+        public DbSet<Usuario> Usuario { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new ClienteMap());
+            modelBuilder.Configurations.Add(new ExtraMap());
+            modelBuilder.Configurations.Add(new LocacaoMap());
+            modelBuilder.Configurations.Add(new PacoteMap());
+            modelBuilder.Configurations.Add(new ProdutoConsoleMap());
+            modelBuilder.Configurations.Add(new UsuarioMap());
+            // base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
---
+++ modulo06_dotnet/ProjetoFinal/backend/LocadoraCrescer/LocadoraCrescer.Infraestrutura/Mappings/ExtraMap.cs
@@ -0,0 +1,18 @@
+﻿using LocadoraCrescer.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraCrescer.Infraestrutura.Mappings
+{
+    class ExtraMap : EntityTypeConfiguration<Extra>
+    {
+        public ExtraMap()
+        {
+            ToTable("Extra");
+        }
+    }
+}
