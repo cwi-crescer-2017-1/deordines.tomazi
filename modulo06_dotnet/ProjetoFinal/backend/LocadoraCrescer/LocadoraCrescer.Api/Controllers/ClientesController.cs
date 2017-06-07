9806f6b1f262a217e7a64b45734b0dffc6f815bb
deordines <deordines.tomazi@cwi.com.br>
2017-06-06 21:58:56 -0300

    feat(LocadoraCrescer): adicionar repositório e controller da entidade Cliente.

---
+++ modulo06_dotnet/ProjetoFinal/backend/LocadoraCrescer/LocadoraCrescer.Api/Controllers/ClientesController.cs
@@ -0,0 +1,31 @@
+﻿using LocadoraCrescer.Dominio.Entidades;
+using LocadoraCrescer.Infraestrutura.Repositorios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LocadoraCrescer.Api.Controllers
+{
+    [RoutePrefix("api/clientes")]
+    public class ClientesController : ApiController
+    {
+        private ClienteRepositorio repositorio = new ClienteRepositorio();
+
+        [HttpGet]
+        public IHttpActionResult Listar()
+        {
+            var clientes = repositorio.Listar();
+            return Ok(new { dados = clientes });
+        }
+
+        [HttpPost]
+        public IHttpActionResult Criar(Cliente cliente)
+        {
+            repositorio.Criar(cliente.Nome, cliente.CPF, cliente.DataNascimento, cliente.Endereco, cliente.Genero);
+            return Ok(new { dados = cliente });
+        }
+    }
+}
---
+++ modulo06_dotnet/ProjetoFinal/backend/LocadoraCrescer/LocadoraCrescer.Infraestrutura/Repositorios/ClienteRepositorio.cs
@@ -0,0 +1,33 @@
+﻿using LocadoraCrescer.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraCrescer.Infraestrutura.Repositorios
+{
+    public class ClienteRepositorio
+    {
+        private Contexto contexto = new Contexto();
+
+        public void Criar(string nome, string cpf, DateTime dataNascimento, string endereco, Genero genero)
+        {
+            /*{
+                "Nome": "Nome Cliente",
+                "CPF": 01234567890,
+                "DataNascimento": "1990-01-01",
+                "Endereco": "Av. de Teste, 0123",
+                "Genero": 0 - 1 - 2
+            }*/
+            var cliente = new Cliente(nome, cpf, dataNascimento, endereco, genero);
+            contexto.Cliente.Add(cliente);
+            contexto.SaveChanges();
+        }
+
+        public List<Cliente> Listar()
+        {
+            return contexto.Cliente.ToList();
+        }
+    }
+}
