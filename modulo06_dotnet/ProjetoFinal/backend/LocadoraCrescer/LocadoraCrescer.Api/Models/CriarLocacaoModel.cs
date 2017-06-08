c50e40c78ff3ef823a835126b431842a9464471a
deordines <deordines.tomazi@cwi.com.br>
2017-06-08 20:59:00 -0300

    feat(locadoraCrescer): adicionar models de locacao e pacote.

---
+++ modulo06_dotnet/ProjetoFinal/backend/LocadoraCrescer/LocadoraCrescer.Api/Models/CriarLocacaoModel.cs
@@ -0,0 +1,18 @@
+﻿using LocadoraCrescer.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LocadoraCrescer.Api.Models
+{
+    public class CriarLocacaoModel
+    {
+        public Usuario Usuario { get; set; }
+        public Cliente Cliente { get; set; }
+        public ProdutoConsole ProdutoConsole { get; set; }
+        public Pacote Pacote { get; set; }
+        //public PacoteExtra PacoteExtra { get; set; }
+        public DateTime DataPedido { get; set; }
+    }
+}
---
+++ modulo06_dotnet/ProjetoFinal/backend/LocadoraCrescer/LocadoraCrescer.Api/Models/CriarPacoteModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LocadoraCrescer.Api.Models
+{
+    public class CriarPacoteModel
+    {
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Valor { get; set; }
+        //public int PacoteExtra { get; set; }
+        public int QuantidadeDias { get; set; }
+
+    }
+}
