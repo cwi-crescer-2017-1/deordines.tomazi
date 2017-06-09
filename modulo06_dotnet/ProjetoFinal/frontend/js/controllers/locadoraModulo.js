a05013f0a413715992728ff22d8f8e52d2ea05f5
deordines <deordines.tomazi@cwi.com.br>
2017-06-09 05:32:24 -0300

    feat(locadoraCrescer): injetar ngStorage e auth. Configurar modulo authConfig e as rotas com autenticação.

--- modulo06_dotnet/ProjetoFinal/frontend/js/controllers/locadoraModulo.js
+++ modulo06_dotnet/ProjetoFinal/frontend/js/controllers/locadoraModulo.js
@@ -1,7 +1,31 @@
-var modulo = angular.module('locadoraCrescerApp', ['ngRoute']);
+var modulo = angular.module('locadoraCrescerApp', ['ngRoute', 'ngStorage', 'auth']);
+
+modulo.constant('authConfig', {
+
+    // Obrigatória - URL da API que retorna o usuário
+    //urlUsuario: 'http://10.99.3.24/AutDemo.WebApi/api/acessos/usuario',
+    urlUsuario: 'http://localhost:59655/api/acessos/usuario',
+
+    // Obrigatória - URL da aplicação que possui o formulário de login
+    urlLogin: '/login',
+
+    // Opcional - URL da aplicação para onde será redirecionado (se for informado) após o LOGIN com sucesso
+    urlPrivado: '/cliente',
+
+    // Opcional - URL da aplicação para onde será redirecionado (se for informado) após o LOGOUT
+    urlLogout: '/home'
+});
 
 modulo.config(function ($routeProvider) {
     $routeProvider
+    .when('/login', {
+        controller: 'loginController',
+        templateUrl: '/templates/login.html'
+    })
+    .when('/home', {
+        controller: 'homeController',
+        templateUrl: '/templates/home.html'
+    })
     .when('/cliente', {
         controller: 'clientesController',
         templateUrl: '/templates/cliente.html'
@@ -18,5 +42,5 @@ modulo.config(function ($routeProvider) {
         controller: 'locacaoController',
         templateUrl: '/templates/locacao.html'
     })
+    .otherwise('/login');
 })
-
