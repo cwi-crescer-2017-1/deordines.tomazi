1314b647273555ecc1dc3277caa8a31e1f2c4799
deordines <deordines.tomazi@cwi.com.br>
2017-06-09 05:39:40 -0300

    fix(locadoraCrescer): alterar tipo da propriedade Permissao de array de string para string.

--- modulo06_dotnet/ProjetoFinal/backend/LocadoraCrescer/LocadoraCrescer.Api/Models/RegistrarUsuarioModel.cs
+++ modulo06_dotnet/ProjetoFinal/backend/LocadoraCrescer/LocadoraCrescer.Api/Models/RegistrarUsuarioModel.cs
@@ -10,6 +10,6 @@ namespace LocadoraCrescer.WebApi.Models
         public string Email { get; set; }
         public string Nome { get; set; }
         public string Senha { get; set; }
-        public string[] Permissao { get; set; }
+        public string Permissao { get; set; }
     }
 }
