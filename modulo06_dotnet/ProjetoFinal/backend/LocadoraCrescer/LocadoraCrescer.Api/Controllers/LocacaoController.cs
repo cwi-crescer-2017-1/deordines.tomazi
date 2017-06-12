6a2840002c3fbb686853e21da70362f868a19937
deordines <deordines.tomazi@cwi.com.br>
2017-06-11 23:23:54 -0300

    feat(locadoraCrescer): adicionar requisição para alugar e devolver uma locação. Implementar o método Dispose.

--- modulo06_dotnet/ProjetoFinal/backend/LocadoraCrescer/LocadoraCrescer.Api/Controllers/LocacaoController.cs
+++ modulo06_dotnet/ProjetoFinal/backend/LocadoraCrescer/LocadoraCrescer.Api/Controllers/LocacaoController.cs
@@ -13,12 +13,13 @@ namespace LocadoraCrescer.Api.Controllers
 {
     [BasicAuthorization]
     [RoutePrefix("api/locacao")]
-    public class LocacaoController : ApiController
+    public class LocacaoController : ApiController, IDisposable
     {
         private LocacaoRepositorio repositorioLocacao = new LocacaoRepositorio();
         private ClienteRepositorio repositorioCliente = new ClienteRepositorio();
         private ProdutoConsoleRepositorio repositorioProduto = new ProdutoConsoleRepositorio();
         private PacoteRepositorio repositorioPacote = new PacoteRepositorio();
+        private ExtraRepositorio repositorioExtra = new ExtraRepositorio();
 
         [Authorize(Roles = "funcionario")]
         [HttpGet]
@@ -35,12 +36,71 @@ namespace LocadoraCrescer.Api.Controllers
             var cliente = repositorioCliente.BuscarPorId(model.IdCliente);
             var produto = repositorioProduto.BuscarPorId(model.IdProdutoConsole);
             var pacote = repositorioPacote.BuscarPorId(model.IdPacote);
+            //var extra = repositorioExtra.BuscarPorId(model.IdExtra);
             var locacao = new Locacao(cliente, produto, pacote);
+
+            repositorioProduto.Alugar(produto);
+
+            var extraJogo = repositorioExtra.BuscarPorId(1);
+            var extraControle = repositorioExtra.BuscarPorId(2);
+            switch (pacote.Id)
+            {
+                case 1:
+                    repositorioExtra.Alugar(extraJogo, 1);
+                    repositorioExtra.Alugar(extraControle, 1);
+                    break;
+                case 2:
+                    repositorioExtra.Alugar(extraJogo, 2);
+                    repositorioExtra.Alugar(extraControle, 1);
+                    break;
+                case 3:
+                    repositorioExtra.Alugar(extraJogo, 5);
+                    repositorioExtra.Alugar(extraControle, 2);
+                    break;
+            }
+
             repositorioLocacao.Criar(locacao);
             return Ok(new { dados = locacao });
         }
 
-        //[HttpPut]
-        //public IHttpActionResult
+        [Authorize(Roles = "funcionario")]
+        [HttpPut]
+        [Route("devolver/")]
+        public IHttpActionResult Devolver([FromBody]AlterarPedido model)
+        {
+            var locacao = repositorioLocacao.BuscarPorId(model.Id);
+
+            repositorioProduto.Devolver(locacao.ProdutoConsole);
+
+            var extraJogo = repositorioExtra.BuscarPorId(1);
+            var extraControle = repositorioExtra.BuscarPorId(2);
+            switch (locacao.Pacote.Id)
+            {
+                case 1:
+                    repositorioExtra.Devolver(extraJogo, 1);
+                    repositorioExtra.Devolver(extraControle, 1);
+                    break;
+                case 2:
+                    repositorioExtra.Devolver(extraJogo, 2);
+                    repositorioExtra.Devolver(extraControle, 1);
+                    break;
+                case 3:
+                    repositorioExtra.Devolver(extraJogo, 5);
+                    repositorioExtra.Devolver(extraControle, 2);
+                    break;
+            }
+
+            repositorioLocacao.Devolver(locacao);
+            return Ok(new { dados = locacao });
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            repositorioCliente.Dispose();
+            repositorioProduto.Dispose();
+            repositorioPacote.Dispose();
+            repositorioLocacao.Dispose();
+            base.Dispose(disposing);
+        }
     }
 }
