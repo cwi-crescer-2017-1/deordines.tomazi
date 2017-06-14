b7c5e1d0013e309fd7c5356f44859690878133e7
deordines <deordines.tomazi@cwi.com.br>
2017-06-14 02:59:13 -0300

    feat(oracle): resolução exercício 2.

--- modulo07_oracle/Aula_02/Oracle_Lista_2.sql
+++ modulo07_oracle/Aula_02/Oracle_Lista_2.sql
@@ -2,23 +2,57 @@
 -- Cidades Duplicadas
 -- Atualmente a tabela de Cidade tem registros duplicados (nome e UF). Faça um código (PL/SQL) que liste quais são as cidades duplicadas. Após isso liste todos os clientes que estão relacionados com estas cidades
 DECLARE
-  CURSOR ListaClientes IS
-    SELECT cli.IDCIdade, cli.Nome, cid.Nome as NomeCidade
-    FROM Cliente cli
-    JOIN Cidade cid ON cid.IDCidade = cli.IDCidade;
+    CURSOR ListaClientes IS
+        SELECT cli.IDCIdade, cli.Nome, cid.Nome as NomeCidade
+        FROM Cliente cli
+        JOIN Cidade cid ON cid.IDCidade = cli.IDCidade;
     
-  CURSOR ListaDeCidadesRepetidas IS
-    SELECT Nome, UF, COUNT(*)
-    FROM Cidade
-    GROUP BY Nome, UF
-    HAVING COUNT(*) > 1
-    ORDER BY Nome;
+    CURSOR ListaDeCidadesRepetidas IS
+        SELECT Nome, UF, COUNT(*)
+        FROM Cidade
+        GROUP BY Nome, UF
+        HAVING COUNT(*) > 1
+        ORDER BY Nome;
 BEGIN
-  FOR cidade IN ListaDeCidadesRepetidas LOOP
-    FOR cliente IN ListaClientes LOOP
-        IF (cidade.Nome = cliente.NomeCidade) THEN
-            DBMS_OUTPUT.PUT_LINE(cliente.nome);
-        END IF;
+    FOR cidade IN ListaDeCidadesRepetidas LOOP
+        FOR cliente IN ListaClientes LOOP
+            IF (cidade.Nome = cliente.NomeCidade) THEN
+                DBMS_OUTPUT.PUT_LINE(cliente.nome);
+            END IF;
+        END LOOP;
     END LOOP;
-  END LOOP;
+END;
+
+-- Exercício 2
+-- Atualizando Valor do Pedido
+-- Faça uma rotina que permita atualizar o valor do pedido a partir dos seus itens. Esta rotina deve receber por parametro o IDPedido e então verificar o valor total de seus itens (quantidade x valor unitário).
+DECLARE
+    vValorAtualizado    Pedido.ValorPedido%TYPE;
+    vId                 Pedido.IdPedido%TYPE;
+
+    --CURSOR Pedido (Id2 in NUMBER) IS
+        --SELECT ValorPedido
+        --FROM Pedido
+        --WHERE IdPedido = Id2;
+        
+    CURSOR PedidoItem (Id in NUMBER) IS
+        SELECT pedItem.Quantidade, pedItem.PrecoUnitario
+        FROM PedidoItem pedItem
+        WHERE pedItem.IdPedido = Id;
+BEGIN
+    vId := :Id;
+    vValorAtualizado := 0;
+
+    --FOR item2 IN Pedido(vId) LOOP
+        --DBMS_OUTPUT.PUT_LINE(item2.ValorPedido);
+    --END LOOP;
+    
+    FOR item IN PedidoItem(vId) LOOP
+        vValorAtualizado := vValorAtualizado + (item.Quantidade * item.PrecoUnitario);
+        DBMS_OUTPUT.PUT_LINE(vValorAtualizado);
+    END LOOP;
+    
+    UPDATE Pedido
+    SET ValorPedido = vValorAtualizado
+    WHERE IdPedido = vId;
 END;
