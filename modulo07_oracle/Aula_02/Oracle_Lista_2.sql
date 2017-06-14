2588452c298536bb17138ddf29d76e47b165b82f
deordines <deordines.tomazi@cwi.com.br>
2017-06-14 01:20:11 -0300

    feat(oracle): complementar exercício 1.

--- modulo07_oracle/Aula_02/Oracle_Lista_2.sql
+++ modulo07_oracle/Aula_02/Oracle_Lista_2.sql
@@ -3,25 +3,22 @@
 -- Atualmente a tabela de Cidade tem registros duplicados (nome e UF). Faça um código (PL/SQL) que liste quais são as cidades duplicadas. Após isso liste todos os clientes que estão relacionados com estas cidades
 DECLARE
   CURSOR ListaClientes IS
-    SELECT cli.IDCIdade, cli.Nome
+    SELECT cli.IDCIdade, cli.Nome, cid.Nome as NomeCidade
     FROM Cliente cli
     JOIN Cidade cid ON cid.IDCidade = cli.IDCidade;
-  CURSOR ListaCidades IS
-    SELECT IDCidade, Nome, UF, COUNT(*)
+    
+  CURSOR ListaDeCidadesRepetidas IS
+    SELECT Nome, UF, COUNT(*)
     FROM Cidade
-    GROUP BY IDCIdade, Nome, UF
+    GROUP BY Nome, UF
     HAVING COUNT(*) > 1
     ORDER BY Nome;
 BEGIN
-  FOR cidade IN ListaCidades LOOP
-    DBMS_OUTPUT.PUT_LINE(cidade.nome);
-  END LOOP;
-  
-  --FOR cliente IN ListaClientes LOOP
-    FOR cidade IN ListaCidades LOOP
-      --IF (cliente.IDCidade = cidade.IDCidade) THEN 
-        DBMS_OUTPUT.PUT_LINE(cidade.Nome);
-      --END IF;
+  FOR cidade IN ListaDeCidadesRepetidas LOOP
+    FOR cliente IN ListaClientes LOOP
+        IF (cidade.Nome = cliente.NomeCidade) THEN
+            DBMS_OUTPUT.PUT_LINE(cliente.nome);
+        END IF;
     END LOOP;
-  --END LOOP;
+  END LOOP;
 END;
