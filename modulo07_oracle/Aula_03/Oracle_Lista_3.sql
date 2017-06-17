15b0c8d5b1eb1787781e4fde2a9929e7dccc20ff
deordines <deordines.tomazi@cwi.com.br>
2017-06-17 05:20:30 -0300

    feat(oracle): complementar exerício da lista 3.

--- modulo07_oracle/Aula_03/Oracle_Lista_3.sql
+++ modulo07_oracle/Aula_03/Oracle_Lista_3.sql
@@ -8,48 +8,53 @@
 -- -> certificando-se que não há mais clientes utilizando cidades duplicadas elimine-as.
 -- Utilize uma codificação que possibilite o uso de teste unitário.
 
-create or replace PACKAGE pck_cidade AS
+CREATE OR REPLACE PACKAGE pck_cidade AS
     PROCEDURE IdentificarCidadesDuplicadas;
-    FUNCTION BuscarCidadeComMenorId(pNome VARCHAR2, pUF VARCHAR2) return INTEGER;
-END
+    FUNCTION BuscarCidadeComMenorId(pNome IN VARCHAR2, pUF IN VARCHAR2) return INTEGER;
+END;
 
-create or replace PACKAGE BODY pck_cidade AS
-    vNovoId Cidade.IdCidade%TYPE;
-
-    FUNCTION BuscarCidadeComMenorId(pNome VARCHAR2, pUF VARCHAR2) return INTEGER AS
+CREATE OR REPLACE PACKAGE BODY pck_cidade AS
+    FUNCTION BuscarCidadeComMenorId(pNome IN VARCHAR2, pUF IN VARCHAR2) return INTEGER AS
         vId Cidade.IdCidade%TYPE;
         BEGIN
             SELECT MIN(IdCidade) as MenorID
             INTO vId
             FROM Cidade
-            WHERE Cidade.Nome = pNome
-            AND Cidade.UF = pUF;
+            WHERE Nome = pNome
+            AND UF = pUF;
         RETURN vId;
     END BuscarCidadeComMenorId;
-
+    
     PROCEDURE IdentificarCidadesDuplicadas AS
+        vNovoIdCidade Cidade.IdCidade%TYPE;
+        
         CURSOR ListaDeCidadesRepetidas IS
-            SELECT Nome, UF, COUNT(*)
+            SELECT Nome, UF
             FROM Cidade
             GROUP BY Nome, UF
             HAVING COUNT(*) > 1
             ORDER BY Nome;
 
         CURSOR ListaClientes (pNome IN VARCHAR2, pUF IN VARCHAR2) IS
-            SELECT cli.IdCliente, cli.Nome as NomeCliente, cid.Nome as NomeCidade, cid.UF
+            SELECT cli.IdCliente, cli.Nome as NomeCliente, cid.Nome as NomeCidade, cid.UF, cid.IdCidade
             FROM Cliente cli
             JOIN Cidade cid ON cid.IdCidade = cli.IdCidade
             WHERE cid.Nome = pNome
             AND cid.UF = pUF;
         BEGIN
             FOR cidade IN ListaDeCidadesRepetidas LOOP
-                DBMS_OUTPUT.PUT_LINE(cidade.Nome);
-                vNovoId := pck_cidade.BuscarCidadeComMenorId(cidade.Nome, cidade.UF);
+                vNovoIdCidade := pck_cidade.BuscarCidadeComMenorId(cidade.Nome, cidade.UF);
+                DBMS_OUTPUT.PUT_LINE(cidade.Nome || ' - ' || vNovoIdCidade);
                 FOR cliente IN ListaClientes(cidade.Nome, cidade.UF) LOOP
-                    UPDATE cliente
-                    SET IdCidade = vNovoId
-                    WHERE IdCidade != vNovoId;
+                    DBMS_OUTPUT.PUT_LINE(cliente.IdCliente || ' - ' || cliente.NomeCliente || ' - (' || cliente.IdCidade || ')');
+                    UPDATE Cliente
+                    SET IdCidade = vNovoIdCidade
+                    WHERE IdCliente = cliente.IdCliente;
                 END LOOP;
+                DELETE FROM Cidade
+                WHERE Nome = cidade.Nome
+                AND UF = cidade.UF
+                AND IdCidade != vNovoIdCidade;
             END LOOP;
     END IdentificarCidadesDuplicadas;
 END pck_cidade;
