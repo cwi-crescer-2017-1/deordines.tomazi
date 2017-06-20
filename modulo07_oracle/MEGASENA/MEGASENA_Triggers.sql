b54069001d28919f471b767d59e2bd3b6b4409ba
deordines <deordines.tomazi@cwi.com.br>
2017-06-19 23:16:00 -0300

    refactor(oracle): finalizar implementação do exercício 1 com triggers e remover código comentado com erros.

--- modulo07_oracle/MEGASENA/MEGASENA_Triggers.sql
+++ modulo07_oracle/MEGASENA/MEGASENA_Triggers.sql
@@ -6,66 +6,79 @@ A auditoria deve monitorar tanto as apostas quanto os números de cada aposta.
 */
 
 CREATE TABLE Log_Aud_Apostas (
-    IdAudApostas   INTEGER NOT NULL,
-    Aposta         INTEGER NOT NULL,
-    --NumeroAposta   INTEGER NOT NULL,
-    Data           DATE DEFAULT SYSDATE,
-    Operacao       CHAR(1) NOT NULL,
-    CONSTRAINT PK_Log_Aud_Apostas PRIMARY KEY (IdAudApostas)--,
-    --CONSTRAINT FK_Aposta FOREIGN KEY (Aposta) REFERENCES Aposta(IdAposta),
-    --CONSTRAINT FK_NumeroAposta FOREIGN KEY (NumeroAposta) REFERENCES Numero_Aposta(IdNumero_Aposta)
+    IdAudApostas    INTEGER NOT NULL,
+    ApostaAntiga    INTEGER,
+    ApostaNova      INTEGER,
+    Data            DATE DEFAULT SYSDATE,
+    Operacao        CHAR(1) NOT NULL,
+    CONSTRAINT PK_Log_Aud_Apostas PRIMARY KEY (IdAudApostas)
 );  
 
 CREATE TABLE Log_Aud_Numeros (
-    IdAudNumeros  INTEGER NOT NULL,
-    Aposta        INTEGER NOT NULL,
-    Numero        INTEGER NOT NULL,
+    IdAudNumeros    INTEGER NOT NULL,
+    IdAposta        INTEGER NOT NULL,
+    NumeroAntigo    INTEGER NOT NULL,
+    NumeroNovo      INTEGER NOT NULL,
+    Data            DATE DEFAULT SYSDATE,
     CONSTRAINT PK_Log_Aud_Numeros PRIMARY KEY (IdAudNumeros),
-    CONSTRAINT FK_Numero FOREIGN KEY (Numero) REFERENCES Numero_Aposta(IdNumero_Aposta)
+    CONSTRAINT Aposta FOREIGN KEY (IdAposta) REFERENCES Aposta(IdAposta)
 );
     
 CREATE SEQUENCE sqLog_Aud_Apostas;
 CREATE SEQUENCE sqLog_Aud_Numeros;
 
 CREATE OR REPLACE TRIGGER TR_AUD_APOSTA
-    AFTER INSERT OR UPDATE OR DELETE ON APOSTA
-DECLARE
-    vOperacao   CHAR(1);
-    vIdAposta   INTEGER NOT NULL;
-BEGIN
-    vIdAposta := :new.IdAposta;
-    
-    IF INSERTING THEN
-        vOperacao := 'I';
-    END IF;
-    
-    INSERT INTO Log_Aud_Apostas (iddAudApostas, aposta, data)
-    VALUES (sqLog_Aud_Apostas.nextVal, vIdAposta, sysdate);
-END TR_AUD_APOSTA;
-    
-/*
-CREATE OR REPLACE TRIGGER TR_AUD1_APOSTA
     AFTER INSERT OR UPDATE OR DELETE ON Aposta
     FOR EACH ROW
 DECLARE
-    v_operacao      CHAR(1);
-    v_idAposta      INTEGER;
-    v_numeroAposta  INTEGER;
-BEGIN      
-    SELECT MAX(IdAposta)
-    INTO v_idAposta
-    FROM Aposta;
-    
-    IF INSERTING THEN        
-        v_operacao := 'I';
+    vApostaAntiga   INTEGER;
+    vApostaNova     INTEGER;
+    vOperacao       CHAR(1);
+BEGIN    
+    IF INSERTING THEN
+        vApostaAntiga := NULL;
+        vApostaNova := :new.IdAposta;
+        vOperacao := 'I';
+    ELSIF UPDATING THEN
+        vApostaAntiga := :old.IdAposta;
+        vApostaNova := :new.IdAposta;
+        vOperacao := 'U';
+    ELSE
+        vApostaAntiga := :old.IdAposta;
+        vApostaNova := NULL;
+        vOperacao := 'D';
     END IF;
     
-    SELECT IdNumero_Aposta
-    INTO v_NumeroAposta
-    FROM Numero_Aposta
-    WHERE IdAposta = v_Aposta;
+    INSERT INTO Log_Aud_Apostas (IdAudApostas, ApostaAntiga, ApostaNova, Data, Operacao)
+        VALUES (sqLog_Aud_Apostas.nextVal, vApostaAntiga, vApostaNova, SYSDATE, vOperacao);
+END TR_AUD_APOSTA;
+
+CREATE OR REPLACE TRIGGER TR_AUD_NUMEROS
+    AFTER INSERT OR UPDATE OR DELETE ON Numero_Aposta
+    FOR EACH ROW
+DECLARE
+    vAposta         INTEGER;
+    vNumeroAntigo   INTEGER;
+    vNumeroNovo     INTEGER;
+    vOperacao       CHAR(1);
+BEGIN    
+    IF INSERTING THEN
+        vAposta := :new.Aposta;
+        vNumeroAntigo := NULL;
+        vNumeroNovo := :new.Numero;
+        vOperacao := 'I';
+    ELSIF UPDATING THEN
+        vAposta := :old.Aposta;
+        vNumeroAntigo := :old.Numero;
+        vNumeroNovo := :new.Numero;
+        vOperacao := 'U';
+    ELSE
+        vAposta := :old.Aposta;
+        vNumeroAntigo := :old.Numero;
+        vNumeroNovo := NULL;
+        vOperacao := 'D';
+    END IF;
     
-    INSERT INTO Log_Aud_Apostas (idAudApostas, aposta, numeroAposta, data, operacao)
-    VALUES (Log_Aud_Apostas.nextVal, v_Aposta, v_NumeroAposta, sysdate, v_operacao);
-End TR_AUD1_APOSTA;
-*/
+    INSERT INTO Log_Aud_Apostas (IdAudApostas, Aposta, NumeroAntigo, NumeroNovo, Data, Operacao)
+        VALUES (sqLog_Aud_Apostas.nextVal, vAposta, vNumeroAntigo, vNumeroNovo, SYSDATE, vOperacao);
+END TR_AUD_NUMEROS;
