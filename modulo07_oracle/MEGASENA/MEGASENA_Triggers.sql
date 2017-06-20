2958645c2b4f45c024a70927e4853d7bce8bff2a
deordines <deordines.tomazi@cwi.com.br>
2017-06-19 23:55:06 -0300

    feat(oracle): adicionar resolução do exerício 2 da lista 4.

--- modulo07_oracle/MEGASENA/MEGASENA_Triggers.sql
+++ modulo07_oracle/MEGASENA/MEGASENA_Triggers.sql
@@ -82,3 +82,24 @@ BEGIN
     INSERT INTO Log_Aud_Apostas (IdAudApostas, Aposta, NumeroAntigo, NumeroNovo, Data, Operacao)
         VALUES (sqLog_Aud_Apostas.nextVal, vAposta, vNumeroAntigo, vNumeroNovo, SYSDATE, vOperacao);
 END TR_AUD_NUMEROS;
+
+/*Exercício 2
+TOP Estados
+Liste os estados com maior número de apostas, e maior valor arrecadado em cada concurso.
+Adicionalmente também deve ser exibido o total de acertadores e o valor da premiação em cada estado.*/
+
+CREATE OR REPLACE VIEW Top_Estados_Apostas AS
+SELECT  cid.UF AS Estado,
+        con.IdConcurso AS Concurso,
+        SUM(apst.IdAposta) AS Quantidade_Apostas,
+        SUM(apst.Valor) AS Valor_Total_Apostas,
+        SUM(apstP.IdAposta_Premiada) AS Quantidade_Acertadores,
+        SUM(apstP.Valor) AS Valor_Total_Acertadores
+FROM Aposta apst
+INNER JOIN Cidade cid ON cid.IdCidade = apst.IdCidade
+INNER JOIN Concurso con ON con.IdConcurso = apst.IdConcurso
+LEFT JOIN Aposta_Premiada apstP ON apstP.IdAposta = apst.IdAposta
+GROUP BY cid.UF, con.IdConcurso
+ORDER BY Valor_Total_Apostas DESC;
+
+SELECT * FROM Top_Estados_Apostas;
