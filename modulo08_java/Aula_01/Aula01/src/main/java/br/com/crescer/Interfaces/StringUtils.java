244038e51d8156e66ca72f6331bd866c74d39562
deordines <deordines.tomazi@cwi.com.br>
2017-06-20 23:24:36 -0300

    feat(java): adicionar interfaces da lista de exercício 1.

---
+++ modulo08_java/Aula_01/Aula01/src/main/java/br/com/crescer/Interfaces/StringUtils.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.crescer.Interfaces;
+
+/**
+ *
+ * @author Deordines
+ */
+public interface StringUtils {
+    
+    boolean isEmpty(String string);
+    String inverter(String string);    
+    int contaVogais(String string);    
+    boolean isPalindromo(String string);    
+}
