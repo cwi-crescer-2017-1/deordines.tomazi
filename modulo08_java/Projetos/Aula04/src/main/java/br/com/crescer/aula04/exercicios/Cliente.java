e519c67961a6822e39c0b374bec2663d28f11c93
deordines <deordines.tomazi@cwi.com.br>
2017-06-24 23:43:25 -0300

    feat(java): adicionar mapeamento da classe cliente.

---
+++ modulo08_java/Projetos/Aula04/src/main/java/br/com/crescer/aula04/exercicios/Cliente.java
@@ -0,0 +1,235 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.crescer.aula04.exercicios;
+
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author Deordines
+ */
+
+@Entity
+@Table(name = "CLIENTE")
+public class Cliente {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_CLIENTE")
+    @SequenceGenerator(name = "SEQ_CLIENTE", sequenceName = "SEQ_CLIENTE", allocationSize = 1)
+    @Column(name = "ID", length = 12, nullable = false)
+    private Long id;
+    
+    @Column(name = "NOME", length = 50, nullable = false)
+    private String nome;
+    
+    @Column(name = "CPF", length = 11, nullable = false)
+    private String cpf;
+    
+    @Column(name = "RG", length = 15, nullable = false)
+    private String rg;
+    
+    @Column(name = "RUA", length = 50)
+    private String rua;
+    
+    @Column(name = "BAIRRO", length = 50)
+    private String bairro;
+    
+    @Column(name = "CIDADE", length = 50)
+    private String cidade;
+
+    @Column(name = "NUMERO_CASA", length = 50)
+    private String numeroCasa;
+    
+    @Column(name = "EMAIL", length = 50)
+    private String email;
+
+    @Column(name = "TELEFOME", length = 50)
+    private String telefone;
+    
+    @Column(name = "CELULAR", length = 50, nullable = false)
+    private String celular;
+    
+    @Column(name = "NASCIMENTO")
+    @Temporal(TemporalType.DATE)
+    private Date nascimento;
+
+    /**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the cpf
+     */
+    public String getCpf() {
+        return cpf;
+    }
+
+    /**
+     * @param cpf the cpf to set
+     */
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    /**
+     * @return the rg
+     */
+    public String getRg() {
+        return rg;
+    }
+
+    /**
+     * @param rg the rg to set
+     */
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    /**
+     * @return the rua
+     */
+    public String getRua() {
+        return rua;
+    }
+
+    /**
+     * @param rua the rua to set
+     */
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    /**
+     * @return the bairro
+     */
+    public String getBairro() {
+        return bairro;
+    }
+
+    /**
+     * @param bairro the bairro to set
+     */
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    /**
+     * @return the cidade
+     */
+    public String getCidade() {
+        return cidade;
+    }
+
+    /**
+     * @param cidade the cidade to set
+     */
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    /**
+     * @return the numeroCasa
+     */
+    public String getNumeroCasa() {
+        return numeroCasa;
+    }
+
+    /**
+     * @param numeroCasa the numeroCasa to set
+     */
+    public void setNumeroCasa(String numeroCasa) {
+        this.numeroCasa = numeroCasa;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the telefone
+     */
+    public String getTelefone() {
+        return telefone;
+    }
+
+    /**
+     * @param telefone the telefone to set
+     */
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    /**
+     * @return the celular
+     */
+    public String getCelular() {
+        return celular;
+    }
+
+    /**
+     * @param celular the celular to set
+     */
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    /**
+     * @return the nascimento
+     */
+    public Date getNascimento() {
+        return nascimento;
+    }
+
+    /**
+     * @param nascimento the nascimento to set
+     */
+    public void setNascimento(Date nascimento) {
+        this.nascimento = nascimento;
+    }
+}
