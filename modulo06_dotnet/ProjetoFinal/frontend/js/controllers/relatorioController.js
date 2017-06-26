ddc000f325e13e5131304f68ac8a3bc1b9b91bf8
deordines <deordines.tomazi@cwi.com.br>
2017-06-26 02:24:31 -0300

    feat(locadoraCrescer): adicionar campo para o valor total no template html. Adicionar conficionais, toastr e loop para somar o valor total das locações.

--- modulo06_dotnet/ProjetoFinal/frontend/js/controllers/relatorioController.js
+++ modulo06_dotnet/ProjetoFinal/frontend/js/controllers/relatorioController.js
@@ -1,18 +1,34 @@
-modulo.controller('relatorioController', function($scope, relatorioService) {
+modulo.controller('relatorioController', function($scope, relatorioService, toastr) {
+    
+    var valorTotal = 0;
 
     $scope.relatorioMensal = [];
     $scope.relatorioAtraso = [];
-    $scope.relatorioAtraso = listarRelatorioAtraso();
+    $scope.relatorioAtraso = listarRelatorioAtraso;
     
     $scope.listarRelatorioMensal = listarRelatorioMensal;
 
     function listarRelatorioMensal(data) {
+        if (angular.isUndefined(data) || data === null) {
+            toastr.error('Ação inválida.');
+        }
         var dataRelatorio = data.data.toJSON().replace(/T.*/, "");
         relatorioService
             .listarRelatorioMensal(dataRelatorio)
             .then(response => {
+                if (response.data.dados.length === 0) {
+                    toastr.info('Sem relatórios no tempo informado.');
+                } else {
+                    toastr.success('Ação realizada com sucesso.');
+                }
                 console.log(response.data.dados);
                 $scope.relatorioMensal = response.data.dados;
+                
+                for(value of $scope.relatorioMensal) {
+                    valorTotal += value.ValorFinal;
+                }
+
+                $scope.valorTotal = valorTotal;
             })
     }
 
@@ -20,6 +36,7 @@ modulo.controller('relatorioController', function($scope, relatorioService) {
         relatorioService
             .listarRelatorioAtraso()
             .then(response => {
+                toastr.success('Ação realizada com sucesso.');
                 console.log(response.data.dados);
                 $scope.relatorioAtraso = response.data.dados;
             })
