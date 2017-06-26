400dcfacda61906a91fd70f6d8dfdcfa72cd5814
deordines <deordines.tomazi@cwi.com.br>
2017-06-26 15:49:50 -0300

    feat(java): adicionar exemplo servlet de get com montagem de html.

---
+++ modulo08_java/Projetos/Aula05/src/main/java/br/com/crescer/aula05/PessoaServlet.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.crescer.aula05;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author deordines.tomazi
+ */
+@WebServlet(name = "pessoa", urlPatterns = {"/pessoa"})
+public class PessoaServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        try (final PrintWriter out = resp.getWriter();) {
+            out.append("<!DOCTYPE html>");
+            out.append("<html>");
+            out.append("<head>");
+            out.append("<title>Java - aula5</title>");
+            out.append("<meta charset=\"UTF-8\">");
+            out.append("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
+            out.append("</head>");
+            out.append("<body>");
+            out.append("<h1>Pessoa</h1>");
+            out.append("</body>");
+            out.append("</html>");
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
