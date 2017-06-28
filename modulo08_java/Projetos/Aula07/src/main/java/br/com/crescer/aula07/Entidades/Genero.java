7ef2a46d52a2ec35514f24559ac80ef81778137f
deordines <deordines.tomazi@cwi.com.br>
2017-06-28 17:33:43 -0300

    feat(java): adicionar arquivos da aula 07.

---
+++ modulo08_java/Projetos/Aula07/src/main/java/br/com/crescer/aula07/Entidades/Genero.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.crescer.aula07.Entidades;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Deordines
+ */
+
+@Entity
+//@Table(name = "GENERO")
+public class Genero implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_GENERO")
+    @SequenceGenerator(name = "SEQ_GENERO", sequenceName = "SEQ_GENERO", allocationSize = 1)
+    @Column(name = "ID", length = 12, nullable = false)
+    private Long id;
+    
+    @Column(name = "DESCRICAO", length = 60, nullable = false)
+    private String nome;
+
+    /**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
---
+++ modulo08_java/Projetos/Aula07/src/main/java/br/com/crescer/aula07/Services/GeneroService.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.crescer.aula07.Services;
+
+import br.com.crescer.aula07.Entidades.Genero;
+import br.com.crescer.aula07.Repositories.GeneroRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author deordines.tomazi
+ */
+@Service
+public class GeneroService {
+    
+    @Autowired
+    GeneroRepositorio repositorio;
+    
+    public Iterable<Genero> listar () {
+        return repositorio.findAll();
+    }
+}
