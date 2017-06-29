7e595baab192edcff1ce7c0a04273fe54237a26c
deordines <deordines.tomazi@cwi.com.br>
2017-06-29 15:51:46 -0300

    fix(java): adicionar @Entity e @Table na classe locacao para o mapemento.

--- modulo08_java/Projetos/Aula07/src/main/java/br/com/crescer/aula07/Entidades/Locacao.java
+++ modulo08_java/Projetos/Aula07/src/main/java/br/com/crescer/aula07/Entidades/Locacao.java
@@ -9,12 +9,14 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Date;
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
@@ -22,6 +24,9 @@ import javax.persistence.TemporalType;
  *
  * @author Deordines
  */
+
+@Entity
+@Table(name = "LOCACAO")
 public class Locacao implements Serializable {
     
     @Id
